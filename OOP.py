3ae6f90067ab330f9588d63b1b52ec4c7056318d
Sean Dillon <smdillon@mail.csuchico.edu>
2020-01-25 22:36:02 -0800

    added OOP.py

---
+++ OOP.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 25 21:58:48 2020
+
+@author: seand
+"""
+
+#Object Oriented Programming
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+import astropy as ap
+
+
+
+
+
+
