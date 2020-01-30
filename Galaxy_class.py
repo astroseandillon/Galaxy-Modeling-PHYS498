a15ab8009966c41569abe5556377195253e83d72
Sean Dillon <smdillon@mail.csuchico.edu>
2020-01-29 23:25:01 -0800

    added description of the file, as well as goals we wish to achieve

---
+++ Galaxy_class.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 23:20:27 2020
+
+@author: Seam Dillon
+
+This file is to create an object that we can refer to our gravitational bodies.
+I figure that if we can describe each of galaxies by their conditions, which 
+can help simplify our code.
+"""
+
