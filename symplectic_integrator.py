0104e4a86454421e4d183b846f4fd08b85554555
Sean Dillon <seandillon48@gmail.com>
2020-02-06 10:27:16 -0800

    imported libraries

---
+++ symplectic_integrator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 09:29:22 2020
+
+@author: Sean Dillon
+
+Adding a new integration method using symplectic integration as cited in 
+Donnely and Rogers, 2005
+"""
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
+import astropy as ap
+
+
+
+
+
+
+
