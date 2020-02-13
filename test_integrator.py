06d2851f259c9ca7d491f1fb16010ac7e966845d
Sean Dillon <seandillon48@gmail.com>
2020-02-13 10:28:42 -0800

    created file to test symplectic integrator code

---
+++ test_integrator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 10:26:14 2020
+
+@author: Sean Dillon
+
+Symplectic Integrator Test
+
+This will (hopefully) successfully test our new symplectic integrator code.
+"""
+
